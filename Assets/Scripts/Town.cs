a17886063027533f72a85934c2db8237db6e6616
tyler <tyler@cruftbusters.com>
2022-01-09 20:07:39 -0600

    randomly generate town cells with perlin

--- Assets/Scripts/Town.cs
+++ Assets/Scripts/Town.cs
@@ -12,9 +12,33 @@ public class Town : MonoBehaviour
 
     void Start()
     {
-        List<Vector2> hexes =
-            new List<Vector2>()
-            { new Vector2(-1, 0), new Vector2(0, 0), new Vector2(1, 0) };
+        float scale1 = 1f / 16f;
+        float influence1 = 0.75f;
+        float scale2 = 1f / 64f;
+        float influence2 = 0.25f;
+        int size = 128;
+        float clip = 0.625f;
+        List<Vector2> hexes = new List<Vector2>();
+        for (int q = -size / 2; q < size / 2; q++)
+        {
+            for (int s = -size / 2; s < size / 2; s++)
+            {
+                Vector2 coordinate = new Vector2(q, s);
+                Vector2 world =
+                    coordinateSystem.hexToWorld(coordinate) +
+                    new Vector3(size, size, 0);
+                Vector2 sample1 = world * scale1;
+                Vector2 sample2 = world * scale2;
+                if (
+                    Mathf.PerlinNoise(sample1.x, sample1.y) * influence1 +
+                    Mathf.PerlinNoise(sample2.x, sample2.y) * influence2 >
+                    clip
+                )
+                {
+                    hexes.Add (coordinate);
+                }
+            }
+        }
 
         foreach (Vector3 hex in hexes)
         {
