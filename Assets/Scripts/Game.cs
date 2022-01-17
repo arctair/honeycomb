b044141b02644c944684d3755b62c4e3ded8669c
tyler <tyler@cruftbusters.com>
2022-01-17 07:09:35 -0600

    draw fixed grid and colorize with perlin noise

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -13,19 +13,34 @@ public class Game : MonoBehaviour
 
     void Start()
     {
-        CreateTile(new Vector2(0, 0));
-        CreateTile(new Vector2(1, 0));
-        CreateTile(new Vector2(1, -1));
+        for (int r = 0; r <= 32; r++)
+        {
+            for (int q = 0; q <= 32; q++)
+            {
+                GameObject tile = CreateTile(new Vector2(q - r / 2, r));
+                float sample = Sample(tile.transform.position);
+                tile.GetComponent<Renderer>().material.color =
+                    Color.HSVToRGB(0.125f, 0.75f, sample);
+            }
+        }
     }
 
     private GameObject CreateTile(Vector2 axial)
     {
         Vector2 world =
             new Vector2((axial[0] + axial[1] / 2) * Mathf.Sqrt(2),
-                -axial[1] * Mathf.Sqrt(3) / Mathf.Sqrt(2));
+                -axial[1] * Mathf.Sqrt(3) / Mathf.Sqrt(2)) -
+            new Vector2(20f, -20f);
 
         GameObject tile = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        tile.transform.parent = transform;
         tile.transform.SetPositionAndRotation (world, pointTopRotation);
         return tile;
+    }
+
+    private float Sample(Vector2 world)
+    {
+        Vector2 sample = world / 12f + new Vector2(1024, 1024);
+        return Mathf.PerlinNoise(sample.x, sample.y) * 4f - 3f;
     }
 }
